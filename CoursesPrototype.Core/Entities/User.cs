20681d1b0aacda8ada6518dc1663260c5fb7d0a5
Валерий Иманов <imanov.03.valera@gmail.com>
2023-10-12 21:41:55 +0500

    Add project files.

---
+++ CoursesPrototype.Core/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursesPrototype.Core.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; } = null!;
+        public string HashedPassword { get; set; } = null!;
+        public string Lastname { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string? Patronymic { get; set; }
+    }
+}
