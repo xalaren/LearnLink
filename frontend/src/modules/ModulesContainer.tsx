ed310b4cb7cd914bdfddf5f3e0e759bae40f77b5
Валерий Иманов <imanov.03.valera@gmail.com>
2023-12-17 22:44:24 +0500

    Temporary commit

--- frontend/src/modules/ModulesContainer.tsx
+++ frontend/src/modules/ModulesContainer.tsx
@@ -1,11 +1,50 @@
-function ModulesContainer() {
+import { useState } from "react";
+import { Input } from "../ui/Input";
+import { InputType } from "../models/enums";
+
+interface IModulesContainerProps {
+    allowEdit: boolean;
+}
+
+function ModulesContainer({ allowEdit }: IModulesContainerProps) {
+    const [inputActive, setInputActive] = useState(false);
+    const [moduleTitle, setModuleTitle] = useState('');
+    const [moduleTitleError, setModuleTitleError] = useState('');
+
+    async function onSubmit(event: React.FormEvent) {
+        event.preventDefault();
+    }
+
+    function onChange(event: React.ChangeEvent) {
+        const inputElement = event.target as HTMLInputElement;
+
+        switch (inputElement.name) {
+            case 'moduleTitle':
+                setModuleTitle(inputElement.value);
+                setModuleTitleError('');
+                break;
+        }
+    }
+
     return (
         <div className="course-view__modules">
             <div className="modules__header">
-                <h3>Изучаемые модули:</h3>
-                <div className="modules__container">
+                <h3>Изучаемые модули</h3>
+                <ul className="modules__container">
 
-                </div>
+                </ul>
+
+
+                {allowEdit && <form className='base-form' onSubmit={onSubmit}>
+                    <Input
+                        name="moduleTitle"
+                        onChange={onChange}
+                        type={InputType.text}
+                        width={400}
+                        placeholder="Введите название модуля..."
+                    />
+                </form>
+                }
             </div>
         </div>
     );
